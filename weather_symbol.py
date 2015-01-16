c972df7b92aa520d1df42d05a1875b4651c1faad
Ben Todd <ee10bt@leeds.ac.uk>
2015-01-16 13:18:44 +0000

    added my script to assign weather type a marker and colour. potentially still very buggy at this point, see what you think.

---
+++ weather_symbol.py
@@ -0,0 +1,77 @@
+# MetData - 16/01/15 - Ben
+# Read in weather type, and assign a colour marker for plotting
+# Potentially still very buggy, will find out when plotting?
+
+def weather_symbol(integer):
+
+	# Setup empty vector for color and marker information
+	color_marker=''
+
+	# Assign weather ID a colour and marker
+
+	# Clear night, white circle
+	if integer == 0:
+		color = 'white'
+		marker = 'o'
+
+	# Sunny day, yellow circle
+	elif integer == 1:
+		color = 'yellow'
+		marker = 'o'
+
+	# Cloudy, grey triangle
+	elif integer in [2,3,7,8]:
+		color = 'grey'
+		marker = '^'
+
+	# Misty, grey square
+	elif integer in [5,6]:
+		color = 'grey'
+		marker = 's'
+
+	# Light rain, blue triangle
+	elif integer in [9,10,11,12]:
+		color = 'blue'
+		marker = '^'
+
+	# Heavy rain, dark blue triangle
+	elif integer in [13, 14, 15]:
+		color = 'darkblue'
+		marker = '^'
+
+	# Sleet, light blue triangle
+	elif integer in [16, 17, 18]:
+		color = 'lightblue'
+		marker = '^'
+
+	# Hail, white diamond
+	elif integer in [19, 20, 21]:
+		color = 'white'
+		marker = 'D'
+
+	# Light snow, white hexagon
+	elif integer in [22, 23, 24]:
+		color = 'white'
+		marker = 'h'
+
+	# Heavy snow, grey hexagon
+	elif integer in [25, 26, 27]:
+		color = 'grey'
+		marker = 'h'
+
+	# Thunder, yellow thin diamond
+	elif integer in [28, 29, 30]:
+		color = 'yellow'
+		marker = 'd'
+	
+	# Unspecifed/null weather, black dot
+	else:
+		color = 'black'
+		marker = '.'
+
+	# Fill vector of colour and marker information for weather type
+	color_marker=(color,marker)
+	# Send weather symbol information to plotting script
+	return color_marker
+
+
